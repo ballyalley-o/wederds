1a7ab97d90a159c409f3d9684b6178bd48c57d87
Bally <Bally.Lomibao@howickltd.com>
2024-06-25 13:04:48 +0100

    view model for wederds

---
+++ wederds/ViewModel/WeatherVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wederds.ViewModel
+{
+    public class WeatherVM: INotifyPropertyChanged
+    {
+        private int query;
+
+        public int Query
+        {
+            get { return query; }
+            set 
+            { 
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
